5fc468796868587b9a313f97f283ab758e9c4763
Ilaiyaraja <ilaiyaraja221197@gmail.com>
2024-07-08 17:57:03 +0530

    Added : suppress check is enabled or not.

--- App/Router.php
+++ App/Router.php
@@ -2,23 +2,27 @@
 
 namespace WSPC\App;
 
+use WDR\Core\Helpers\Settings;
+
 defined( "ABSPATH" ) or die();
 
 class Router {
 
-    /**
-     * @return void
-     */
-    public static function init() {
+	/**
+	 * @return void
+	 */
+	public static function init() {
 
-        add_action( 'wp_loaded', function () {
+		add_action( 'wp_loaded', function () {
 
-            if ( \WSPC\App\Controller\Base::check() ) {
-                add_filter( 'wwp_filter_wholesale_price_html', 'WSPC\App\Controller\Base::renderWholeSalePrice', 10, 7 );
-                add_filter( 'wdr_modify_product_price_html', 'WSPC\App\Controller\Base::renderModifiedPrice', 10, 3 );
-            }
-            add_filter( 'wdr_suppress_allowed_hooks', 'WSPC\App\Controller\Base::removeSuppressedHooks', 10, 1 );
+			if ( \WSPC\App\Controller\Base::check() ) {
+				add_filter( 'wwp_filter_wholesale_price_html', 'WSPC\App\Controller\Base::renderWholeSalePrice', 10, 7 );
+				add_filter( 'wdr_modify_product_price_html', 'WSPC\App\Controller\Base::renderModifiedPrice', 10, 3 );
+			}
+			if ( Settings::get( 'suppress_other_discount_plugins' ) ) {
+				add_filter( 'wdr_suppress_allowed_hooks', 'WSPC\App\Controller\Base::removeSuppressedHooks', 10, 1 );
+			}
 
-        } );
-    }
+		} );
+	}
 }
