6a77e4eb5ed4b955c130808433cab85840a56a15
Ilaiyaraja <ilaiyaraja221197@gmail.com>
2024-07-11 10:54:40 +0530

    Added : wholesale suppress

--- App/Router.php
+++ App/Router.php
@@ -19,10 +19,7 @@ class Router {
 				add_filter( 'wwp_filter_wholesale_price_html', 'WSPC\App\Controller\Base::renderWholeSalePrice', 10, 7 );
 				add_filter( 'wdr_modify_product_price_html', 'WSPC\App\Controller\Base::renderModifiedPrice', 10, 3 );
 			}
-			if ( Settings::get( 'suppress_other_discount_plugins' ) ) {
-				add_filter( 'wdr_suppress_allowed_hooks', 'WSPC\App\Controller\Base::removeSuppressedHooks', 10, 1 );
-			}
-
+			add_filter( 'wdr_suppress_allowed_hooks', 'WSPC\App\Controller\Base::removeSuppressedHooks', 10, 1 );
 		} );
 	}
 }
