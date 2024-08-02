fa3b0946b161e300558406fbc330e963af08240f
cartrabbit <saranraj@cartrabbit.in>
2024-08-02 13:29:51 +0530

    Cart adjustment Issue fixed

--- App/Controller/Base.php
+++ App/Controller/Base.php
@@ -2,8 +2,9 @@
 
 namespace WSPC\App\Controller;
 
-
 defined( "ABSPATH" ) or die();
+
+use WDR\Core\Models\Custom\StoreRule;
 
 class Base {
     /**
@@ -16,37 +17,37 @@ class Base {
      *
      * @return bool
      */
-    public static function check() {
-
-        $rules = \WDR\Core\Models\Custom\StoreRule::getRules();
+    public static function check(): bool
+    {
+        $rules = StoreRule::getRules();
         if ( empty( $rules ) ) {
             return false;
         }
-        $res = [];
-        foreach ( $rules as $rule ) {
-            if ( $rule->getDiscountContext() == 'item' && ! in_array( $rule->getType(), array(
-                    'buy_x_get_x',
-                    'buy_x_get_y'
-                ) ) ) {
-                $res[] = $rule;
+        $available_rules = [];
+        foreach ( $rules as $rule )
+        {
+            if ($rule->getDiscountContext() == 'item' && !in_array( $rule->getType(), ['buy_x_get_x', 'buy_x_get_y']))
+            {
+                $available_rules[] = $rule;
             }
         }
 
-        if ( empty( $res ) ) {
+        if ( empty( $available_rules ) ) {
             return false;
         }
-
-
         return true;
     }
 
     /**
-     * @param array $hooks
+     * Removed the suppressed hook
+     *
+     * @param array $hooks list hook name
      *
      * @return array
      */
-    static function removeSuppressedHooks( $hooks ) {
-        if ( empty( $hooks ) || ! is_array( $hooks ) ) {
+    public static function removeSuppressedHooks(array $hooks ): array
+    {
+        if (empty( $hooks )) {
             return $hooks;
         }
         if ( isset( $hooks['woocommerce_get_price_html'] ) ) {
@@ -55,11 +56,12 @@ class Base {
         if ( isset( $hooks['woocommerce_before_calculate_totals'] ) ) {
             unset( $hooks['woocommerce_before_calculate_totals'] );
         }
-
         return $hooks;
     }
 
     /**
+     *  Strikeout the wholesale price for product page.
+     *
      * @param $wholesale_price_html
      * @param $price
      * @param $product
@@ -70,7 +72,7 @@ class Base {
      *
      * @return string
      */
-    static function renderWholeSalePrice( $wholesale_price_html, $price, $product, $user_wholesale_role, $wholesale_price_title_text, $raw_wholesale_price, $source ): string {
+    public static function renderWholeSalePrice( $wholesale_price_html, $price, $product, $user_wholesale_role, $wholesale_price_title_text, $raw_wholesale_price, $source ): string {
         self::$disable_strikeout[ $product->get_id() ] = true;
         if ( $product->is_type( 'grouped' ) ) {
             return $wholesale_price_html;
@@ -90,8 +92,20 @@ class Base {
      *
      * @return bool
      */
-    static function renderModifiedPrice( bool $enable, $product, string $price_html ): bool {
-        return isset( self::$disable_strikeout[ $product->get_id() ] ) && self::$disable_strikeout[ $product->get_id() ] ? false : $enable;
+    public static function renderModifiedPrice( bool $enable, $product, string $price_html ): bool {
+        return is_object($product) && !empty($product) && !empty( self::$disable_strikeout[ $product->get_id() ] ) && self::$disable_strikeout[ $product->get_id() ] ? false : $enable;
     }
 
+    /**
+     *  Get Wholesale price.
+     *
+     * @param $product_price
+     * @param $product
+     * @param $source_id
+     * @param $context
+     * @return mixed
+     */
+    public static function getWholesalePrice($product_price, $product, $source_id, $context) {
+        return is_object($product) && !empty($product) ? $product->get_meta( 'wholesale_customer_wholesale_price', true ) : 0;
+    }
 }
--- App/Router.php
+++ App/Router.php
@@ -2,9 +2,9 @@
 
 namespace WSPC\App;
 
-use WDR\Core\Helpers\Settings;
-
 defined( "ABSPATH" ) or die();
+
+use WSPC\App\Controller\Base;
 
 class Router {
 
@@ -12,14 +12,13 @@ class Router {
 	 * @return void
 	 */
 	public static function init() {
-
 		add_action( 'wp_loaded', function () {
-
-			if ( \WSPC\App\Controller\Base::check() ) {
+			if (Base::check()) {
 				add_filter( 'wwp_filter_wholesale_price_html', 'WSPC\App\Controller\Base::renderWholeSalePrice', 10, 7 );
 				add_filter( 'wdr_modify_product_price_html', 'WSPC\App\Controller\Base::renderModifiedPrice', 10, 3 );
-			}
-			add_filter( 'wdr_suppress_allowed_hooks', 'WSPC\App\Controller\Base::removeSuppressedHooks', 10, 1 );
+            }
+            add_filter( 'wdr_discount_get_product_price', 'WSPC\App\Controller\Base::getWholesalePrice', 10, 4 );
+            add_filter( 'wdr_suppress_allowed_hooks', 'WSPC\App\Controller\Base::removeSuppressedHooks', 10, 1 );
 		} );
 	}
 }
